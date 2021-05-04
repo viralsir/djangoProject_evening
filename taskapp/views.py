b3db3dbb8ef3ff65cf37a9a832423e47b0e99276
viralsir <viralsir2018@gmail.com>
2021-05-04 17:44:44 +0530

    task app is updated with session

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render,redirect
 from django import forms
 # Create your views here.
-tasks=[
-    "check balance",
-    "do recharge",
-    "book ticket",
-    "register user",
-    "do homework"
-]
+
 class NewTaskForm(forms.Form):
     task=forms.CharField(max_length=30,label="New Task :")
     priority=forms.IntegerField(max_value=10,min_value=1)
+    status=forms.ChoiceField(choices=[('active','active'),('deactive','deactive')])
 
 
 def home(request):
+    if "task" not in request.session:
+        request.session["task"]=[]
+
     return render(request,"taskapp/list_task.html",{
-        "tasks":tasks
+        "tasks":request.session["task"]
     })
 
 def add_task(request):
@@ -23,12 +21,14 @@ def add_task(request):
     if request.method == 'POST':
         form=NewTaskForm(request.POST)
         if form.is_valid():
-             tasks.append(form.cleaned_data["task"]);
+             #tasks.append(request.POST["task"]);
+             request.session["task"]+=[form.cleaned_data["task"]];
              return redirect('task-home')
         else :
             return render(request, "taskapp/add_task.html", {
                 "form": form
             })
-    return render(request,"taskapp/add_task.html",{
-        "form":form
-    })
+    else :
+        return render(request,"taskapp/add_task.html",{
+            "form":form
+        })
