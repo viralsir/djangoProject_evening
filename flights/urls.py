d36048a67a859ee4bb2870248eb234cb133be3d0
viralsir <viralsir2018@gmail.com>
2021-05-11 17:42:07 +0530

    book non passenger list is added.

--- flights/urls.py
+++ flights/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home,flight_info
+from .views import home,flight_info,book
 urlpatterns=[
     path("home",home,name="flight-home"),
-    path("details/<int:flight_id>",flight_info,name='flight-details')
+    path("details/<int:flight_id>",flight_info,name='flight-details'),
+    path("book/<int:flight_id>",book,name="flight-book")
+
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import flight
+from django.shortcuts import render,redirect
+from .models import flight,passenger
 # Create your views here.
 def home(request):
     return render(request,"flights/home.html",{
@@ -10,5 +10,14 @@ def flight_info(request,flight_id):
     flightinfo=flight.objects.get(pk=flight_id)
     return render(request,"flights/flight_detail.html",{
         "flight":flightinfo,
-        "passengers":flightinfo.passenger.all()
+        "passengers":flightinfo.passenger.all(),
+        "non_passengers":passenger.objects.exclude(flights=flightinfo).all()
     })
+
+def book(request,flight_id):
+    if request.method=='POST':
+        flightinfo=flight.objects.get(id=flight_id)
+        psgn=passenger.objects.get(id=int(request.POST['non_passenger']))
+        psgn.flights.add(flightinfo)
+        psgn.save()
+        return redirect('flight-home')
